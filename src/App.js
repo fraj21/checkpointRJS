c194f11dc14ac673b648e93c2c9642985902917a
fraj21 <92696071+fraj21@users.noreply.github.com>
2022-02-01 09:00:37 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FoRm from './Components/FoRm';
+
+function App() {
+  return (
+    <div className="App">
+      <FoRm/>
+    </div>
+  );
+}
+
+export default App;
